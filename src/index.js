0051b9a961206b647c2b306ea1a3a6d024b3f0d6
midoarbi <elmehdinassiri31@gmail.com>
2020-06-10 15:57:54 +0100

    evernote

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+const firebase = require('firebase');
+require('firebase/firestore');
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyAclpV5OWv1HoSEmgUnMfFp8bRpMyflCRc",
+    authDomain: "evernote-9be4f.firebaseapp.com",
+    databaseURL: "https://evernote-9be4f.firebaseio.com",
+    projectId: "evernote-9be4f",
+    storageBucket: "evernote-9be4f.appspot.com",
+    messagingSenderId: "374307296458",
+    appId: "1:374307296458:web:f4bd934509f1957d095033"
+  });
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
